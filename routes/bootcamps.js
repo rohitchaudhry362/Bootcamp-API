51e2772d6ddfb9f55530a0078648f5cf7475fa42
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2021-09-09 09:19:13 -0400

    Added pagination for bootcamp and model schema for courses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,10 +4,13 @@ const {
     getBootcamp,
     createBootcamp,
     updateBootcamp,
-    deleteBootcamp
+    deleteBootcamp,
+    getBootcampsInRadius
  } = require('../controllers/bootcamps');
 
 const router = express.Router();
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/')
     .get(getBootcamps)
@@ -17,7 +20,5 @@ router.route('/:id')
     .get(getBootcamp)
     .put(updateBootcamp)
     .delete(deleteBootcamp);
-
-
 
 module.exports = router;
